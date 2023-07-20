cfa8d7385a5dca9257252c8ea927ea227c8d4929
PNYwise <bunayya12345@gmail.com>
2023-07-20 10:27:52 +0700

    add add cities

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 import express from "express";
 import Hashids from "hashids";
 import fs from "fs";
+import 'dotenv/config'
 
 
 // Init express
 const app = express();
-
+const token = process.env.TOKEN
+const apiUrl = process.env.API
 
 const dataPath = './salt.json'
 const setSalt = (data) => {
@@ -74,6 +76,80 @@ app.get("/salt", (req, res) => {
           ...getSalt()
      })
 });
+
+//set city
+app.get("/set-city", async (req, res) => {
+     const datum = [];
+
+     for (let index = 11; index < 95; index++) {
+          const apiUrl = `https://dev.farizdotid.com/api/daerahindonesia/kota?id_provinsi=${index}`;
+          try {
+               // Fetch the data from the API using node-fetch
+               const response = await fetch(apiUrl);
+
+               // Check if the response is successful
+               if (!response.ok) {
+                    throw new Error('Network response was not ok');
+               }
+
+               // Parse the response data as JSON
+               const responseJson = await response.json();
+               const cities = responseJson.kota_kabupaten
+
+               for (const city of cities) {
+                    datum.push(city);
+               }
+          } catch (error) {
+               // Handle any errors that occurred during the fetch
+               console.error('Error fetching data:', error);
+          }
+
+     }
+
+     const newDatum = datum.map((v) => {
+          const str = v?.id;
+          const first = str.toString().slice(0, 2);
+          const last = str.toString().slice(-2);
+          const code = `${first}.${last}`
+          return {
+               code,
+               name: v.nama
+          }
+     })
+     //save to database
+
+     for (const data of newDatum) {
+          fetchCity(data)
+     }
+
+     res.json({
+          message: "success",
+          data: newDatum
+     });
+});
+
+
+
+
+const fetchCity = (body) => {
+     var myHeaders = new Headers();
+     myHeaders.append("Content-Type", "application/json");
+     myHeaders.append("Authorization", `Bearer ${token}`);
+
+     var raw = JSON.stringify(body);
+
+     var requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          redirect: 'follow'
+     };
+
+     fetch(`${apiUrl}/api/v0/master/cities/create`, requestOptions)
+          .then(response => response.text())
+          .then(result => console.log(result))
+          .catch(error => console.log('error', error));
+}
 
 
 // listen on port
